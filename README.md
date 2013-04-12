fd075eb000a7f2f4077a92094d5a57bceb3e9064
bb <bb@xiala.net>
2013-04-12 09:15:23 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,49 @@ jekyll-exiftag
 ==============
 
 A LiquidTag to get Exif Tags using EXIFR
+
+## Usage
+
+```
+{% exiftag tagname,[source],[file] %}
+``` 
+Everything given as tagname is called on EXIFR::JPEG, so this could be model oder f_number.to_f (see https://github.com/remvee/exifr)
+If you give a source, this source is used build the fullpath for the given file (you can also configure them in _config.yml, see below)
+If the file is given, this is the file to get Exif Tags for, this can be alternatively defined in the YAML Front Matter as img: file
+
+
+## Configuration:
+
+Put this in your _config.yml
+``` yaml
+exiftag:
+ sources:
+   - photos
+   - photos/other_source
+```
+These paths are relative to your sites root. Don't add leading and trailing slashes.
+
+
+## License
+
+Copyright (C) 2013 Beni Buess (http://benel.net/)
+
+The MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ jekyll-exiftag.rb
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# A LiquidTag to get Exif Tags using EXIFR
+# by: Beni Buess
+#
+#
+# Usage:
+#
+# {% exiftag tagname,[source],[file] %}
+# 
+# Everything given as tagname is called on EXIFR::JPEG, so this could be model oder f_number.to_f (see https://github.com/remvee/exifr)
+# If you give a source, this source is used build the fullpath for the given file (you can also configure them in _config.yml, see below)
+# If the file is given, this is the file to get Exif Tags for, this can be alternatively defined in the YAML Front Matter as img: file
+#
+#
+# Configuration:
+#
+# Put this in your _config.yml
+#
+# exiftag:
+#  sources:
+#    - photos
+#    - photos/other_source
+#
+# These paths are relative to your sites root. Don't add leading and trailing slashes.
+#
+
+require 'exifr'
+
+module Jekyll
+  class ExifTag < Liquid::Tag
+    def initialize(tag_name, params, token)
+      super
+      @args = self.split_params(params)
+    end
+
+    def render(context)
+      #abort context.registers[:site].config['source'].inspect
+      sources = Array.new(context.registers[:site].config['exiftag']['sources'])
+      # first param is the exif tag
+      tag = @args[0]
+
+      # if a second parameter is passed, use it as a possible img source
+      if @args.count > 1
+        sources.unshift(@args[1])
+      end
+      
+      # the image can be passed as the third parameter
+      if @args.count > 2
+        img = @args[2]
+      # or be defined in the YAML Front Matter like img: <file>
+      else
+        img = context.environments.first["page"]["img"]
+      end
+      
+      # first check if the given img is already the path
+      if File.exist?(img)
+        file_name = img
+      else
+      # then start testing with the sources from _config.yml
+        begin
+          source = sources.shift
+          file_name = File.join(context.registers[:site].config['source'], source, img)
+        end until File.exist?(file_name) or sources.count == 0
+      end
+      # try it and return empty string on failure
+      begin
+        exif = EXIFR::JPEG::new(file_name)
+        "#{exif.instance_eval(tag)}"
+      rescue
+        ""
+      end
+    end
+    
+    def split_params(params)
+      params.split(",").map(&:strip)
+    end
+  end
+end
+
+Liquid::Template.register_tag('exiftag', Jekyll::ExifTag)
